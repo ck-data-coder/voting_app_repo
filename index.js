64c4f88934f29cfb643e00b8746408d1ef030345
chaitanya kaklij <kaklijchaitanya51@gmail.com>
2024-09-14 15:14:25 +0530

    build is created and insert in backend,vercel config

--- index.js
+++ index.js
@@ -4,10 +4,18 @@ import cors from 'cors'
 import bodyParser from 'body-parser'
 import router from './router.js'
 import dotenv from 'dotenv'
+import  path  from 'path'
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 dotenv.config()
 const server =express()
 server.use(cors())
 server.use(bodyParser.json())
 server.use(bodyParser.urlencoded({extended:false}))
-server.use('/',router)
+server.use(express.static(path.join(__dirname,'build')))
+server.use('/api',router)
+server.use('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'build','index.html'))
+})
 server.listen(process.env.PORT)
