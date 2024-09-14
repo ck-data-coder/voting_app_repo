31cc8cb01a934ca8f05e05c2619428a4a76d4b7d
chaitanya kaklij <kaklijchaitanya51@gmail.com>
2024-09-14 21:08:28 +0530

    change route in vercel

--- database.js
+++ database.js
@@ -1,7 +1,10 @@
 import mongoose from "mongoose";
 import dotenv from 'dotenv'
 dotenv.config()
-mongoose.connect(process.env.MONGODB_URL)
+mongoose.connect(process.env.MONGODB_URL, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+  })
 .then(()=>{
     console.log('database connected')
 })
